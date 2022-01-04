e2d203a9e9e296474bbb87d3a7c5a7129f11407c
Kashif Qureshi <kashif.qureshic10@gmail.com>
2022-01-04 16:04:28 +0100

    first commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,55 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  function generateID() {
+    return Math.floor(Math.random() * 100000000);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: generateID(),
+      text, // coming from useState already set to the input field
+      amount: +amount, // coming from useState already set to the input field // + sign will convert it into a number
+    };
+
+    addTransaction(newTransaction);
+  };
+
+  return (
+    <div>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            placeholder="Enter amount..."
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </div>
+  );
+};
